5b70f4b7c29e639289fbed99556e86e81b94ae21
saito <saitou228@gmail.com>
2024-03-19 20:58:00 +0900

    add: 2-12完了

--- plactice_programming/JavaScript/2_JavaScriptの基礎/2-12/null.js
+++ plactice_programming/JavaScript/2_JavaScriptの基礎/2-12/null.js
@@ -21,3 +21,19 @@ let height = 0;
 
 alert(height || 100);
 alert(height ?? 100);
+
+height = null;
+let width = null;
+
+let area = (height ?? 100) * (width ?? 50);
+
+alert(area);
+
+// 下記のようなコードがsyntax errorになる
+// let x = 1 && 2 ?? 3
+
+let x = (1 && 2) ?? 3; // これは動作します。
+alert(x);
+
+// ??は変数にデフォルト値を代入するために使われる
+height = height ?? 100;
