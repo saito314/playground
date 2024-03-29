4d48a5822f78dd96059879b9a4161d0e04ea2233
saito <saitou228@gmail.com>
2024-03-29 23:10:43 +0900

    add: 3-3完了

--- plactice_programming/JavaScript/3_コード品質/3-3/comment.js
+++ plactice_programming/JavaScript/3_コード品質/3-3/comment.js
@@ -32,3 +32,42 @@ function isPrime(n) {
 
     return true;
 }
+
+// コードの塊を関数にする
+for (let i = 0; i < 10; i++) {
+    let drop = getWhiskey();
+    smell(drop);
+    add(drop, glass);
+}
+
+for (let t = 0; t < 3; t++) {
+    let tomato = getTomato();
+    examine(tomato);
+    let juice = press(tomato);
+    add(juice, glass);
+}
+
+addWhiskey(glass);
+addJuice(glass);
+
+function addWhiskey(container) {
+    for(let i = 0; i < 10; i++) {
+        let drop = getWhiskey();
+        //...
+    }
+}
+
+function addJuice(container) {
+    for (let t = 0; t < 3; t++) {
+        let tomato = getTomato();
+        //...
+    }
+}
+
+/**
+  * Returns x raised to the n-th power.
+  * 
+  * @param {number} x The number to raise.
+  * @param {number} n The power, must be a natural number.
+  * @return {number} x raised to the n-th power. 
+  */
