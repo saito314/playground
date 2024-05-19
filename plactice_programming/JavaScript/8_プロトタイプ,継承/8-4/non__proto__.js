e9056321b010c3793078080f26f7242e4d7678bd
saito <saitou228@gmail.com>
2024-05-20 01:42:39 +0900

    update: 8-4完了

--- plactice_programming/JavaScript/8_プロトタイプ,継承/8-4/non__proto__.js
+++ plactice_programming/JavaScript/8_プロトタイプ,継承/8-4/non__proto__.js
@@ -31,13 +31,41 @@
 }
 
 // 非常にシンプルなオブジェクト
+// ユーザから受け取ったkeyに値を入れる場合は__proto__以外はうまくいく
 {
     let obj = {};
 
     let key = prompt("What's the key?", "__proto__");
     obj[key] = "some value";
 
-    alert(obj[key]);
+    alert(obj[key]); // [object, Object], "some value"ではない
 }
 
-// 
+// __proto__は[[Prototype]]にアクセスする方法であり、[[Prototype]]自身ではない
+// 連想配列としてオブジェクトを使いたい場合、リテラルのトリックを使ってそれを行うことができる
+{
+    let obj = Object.create(null);
+
+    let key = prompt("What's the key?", "__proto__");
+    obj[key] = "some value";
+
+    alert(obj[key]); // "some value"
+}
+
+// 上記のようなオブジェクトを純粋な辞書オブジェクトという
+// ただし、組み込みのメソッドはない
+{
+    let obj = Object.create(null);
+
+    alert(obj); // Error
+}
+
+// しかし、連想配列ではとくに問題はない
+// ほとんどのオブジェクトに関連したメソッドObject.keys(obj)などのように機能し続ける
+{
+    let chineseDictionary = Object.create(null);
+    chineseDictionary.hello = "hi hao";
+    chineseDictionary.bye = "zai jian";
+
+    alert(Object.keys(chineseDictionary)); // hello, bye
+}
