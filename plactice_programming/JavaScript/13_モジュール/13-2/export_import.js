288df95f2ee7da577a7a4b0c437348efabff64d2
saito <saitou228@gmail.com>
2024-06-23 18:47:41 +0900

    update: 13-2完了

--- plactice_programming/JavaScript/13_モジュール/13-2/export_import.js
+++ plactice_programming/JavaScript/13_モジュール/13-2/export_import.js
@@ -79,3 +79,93 @@ import User from "./user.js";
 
 new User("John");
 
+// 下記が有効なデフォルトエクスポートの例
+/* クラス名なし
+export default class {
+    constructor() {
+        // ...
+    }
+}
+*/
+
+/* 関数名なし
+export default function (user) {
+    alert(`Hello, ${user}!`);
+}
+*/
+
+/* 変数の作成なしで単一値のエクスポート
+export default ["Jan", "Feb", "Mar", "Apr", "Aug", "Sep", "Oct", "Nov", "Dec"];
+*/
+
+// export defaultはファイル毎に1つだけなのでエラーにはならない
+// ただし、defaultがない場合の名無しはエラーになる
+
+
+// "default"名
+// 状況によってはdefaultキーワードが関数の定義とは別にエクスポートする場合がある
+function sayHi(user) {
+    alert(`Hello, ${user}!`);
+}
+
+// 関数の前に"export default"を追加する
+// export { sayHi as default };
+
+// モジュールuser.jsが1つのメインのデフォルトのものと、幾つかの名前付きのものをエクスポートする場合は次の通り
+/*
+export default class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+export function sayHi(user) {
+    alert(`Hello, ${user}!`);
+}
+*/
+
+// これは名前付きと一緒にデフォルトエクスポートをインポートする方法
+import {default as User, sayHi} from "./user.js";
+
+new User("John");
+
+// そして、オブジェクトとして*ですべてインポートする場合、defaultプロパティはまさにデフォルトエクスポート
+import * as user from "./user.js";
+
+let User = user.default;
+new User("John");
+
+// デフォルトエクスポートを使うべきかどうか
+// 名前付きエクスポートは明示的であり、インポートをするのに正確に正しい名前を使うことを強制する
+// import {User} from "./user.js";
+
+// デフォルトエクスポートの場合はインポート時に常に独自に名前を作成する必要がある
+// import User from "./user.js";
+// import MyUser from "./user.js";
+
+// デフォルトインポートの場合、チームメンバは同じものにことなる名前を使用することができてしまう
+// 通常、それを避けコードの一貫性を保つため、インポートされた変数はファイル名に対応するべきである規則がある
+// import User from "./user.js";
+// import LoginForm from "./loginForm.js";
+// import func from "/path/to/func.js";
+
+
+// 再エクスポート
+// 構文export ... from ...を使用すると別の名前でインポートできる
+export {sayHi} from "./say.js";
+export {default as User} from "./user.js";
+
+// login/logoutをインポートし、すぐにエクスポートする
+// import {login, logout} from "./helper.js";
+// export {login, logout};
+
+// Userとしてdefaultをインポートし、エクスポートする
+// import User from "./user.js";
+// export {User};
+
+// 構文export ... from ...はこのようなインポートエクスポートの短縮記法
+export {login, logout} from "./helper.js";
+
+// Userでdefaultエクスポートを再エクスポート
+export {default as User} from "./user.js";
+
