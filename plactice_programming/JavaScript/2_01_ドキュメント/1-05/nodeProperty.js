1198dc34b4958d7f3347a8fc3d6b2cf7e3911600
saito <saitou228@gmail.com>
2024-07-11 05:01:02 +0900

    update: 2-1-5完了

--- plactice_programming/JavaScript/2_01_ドキュメント/1-05/nodeProperty.js
+++ plactice_programming/JavaScript/2_01_ドキュメント/1-05/nodeProperty.js
@@ -118,3 +118,126 @@
 // 1. 古いコンテンツは削除される
 // 2. 代わりに新しいinnerHTMLが書かれる
 // コンテンツが完全に取り除かれ、最初から書き直されると全ての画像やその他のリソースがリロードされる
+
+
+// outerHTML: 要素の完全なHTML
+// outerHTMLプロパティは要素の完全なHTMLを含む。
+/*
+    // 例
+    <div id="elem">Hello <b>World</b></div>
+
+    <script>
+        alert(elem.outerHTML); // <div id="elem">Hello <b>World</b></div>
+    </script>
+*/
+
+// innerHTMLとは違い、outerHTMLへの書き込みは要素を変更しない
+// 代わりに、DOMで置き換えられる
+/*
+    <div>Hello, World!</div>
+
+    <script>
+        let div = document.querySelector("div");
+
+        // div.outerHTMLを<p>...</p>に置き換え
+        div.outerHTML = "<p>A new element!</p>";
+
+        // なんと! divはまだ同じ
+        alert(div.outerHTML); // <div>Hello, World!</div>
+    </script>
+*/
+
+
+// codeValue/data: テキストノードのコンテンツ
+// innerHTMLプロパティは要素ノードに対してのみ有効
+/*
+    テキストノードのコンテンツ
+    <body>
+        Hello
+        <!-- Comment -->
+        <script>
+            let text = document.body.firstChild;
+            alert(text.data); // Hello
+
+            let comment = text.nextSibling;
+            alert(comment.data); // Comment
+        </script>
+    </body>
+*/
+
+/*
+    開発者がHTMLの中に情報やテンプレートの説明を埋め込むことがある
+    <!-- if isAdmin -->
+    <div>Welcome, Admin!</div>
+    <!-- /if -->
+*/
+
+
+// textContent: 純粋なテキスト
+// textContentは要素内のテキストへのアクセスを提供する
+/*
+    <div id="news">
+        <h1>Headline!</h1>
+        <p>Martians attack people!</p>
+    </div>
+
+    <script>
+        // Headlines! Martians attack people!
+        alert(news.textContent);
+    </script>
+*/
+// textContentへの書き込みは非常に役に立つ
+// なぜならテキストを安全な方法で書くことができるから
+
+// 例えばユーザによって入力された任意の文字列を表示したいとする
+// innerHTMLを使用すると、すべてのHTMLタグを一緒にHTMLとして挿入する
+// textContentを使用すると、全てのシンボルは文字として取り扱われテキストとして挿入される
+/*
+    2つを比較する
+    <div id="elem1"></div>
+    <div id="elem2"></div>
+
+    <script>
+        let name = prompt("What's your name?", "<b>Winnie-the-pooh!</b>");
+
+        elem1.innerHTML = name;
+        elem2.textContent = name;
+    </script>
+*/
+
+// hiddenプロパティ
+// hidden属性とDOMプロパティは要素が見えるかどうかを指定する
+/*
+    <div>Both divs below are hidden</div>
+
+    <div hidden>With the attribute "hidden"</div>
+
+    <div id="elem">JavaScript assigned the property "hidden"</div>
+
+    <script>
+        elem.hidden = true;
+    </script>
+*/
+
+// 技術的にはhiddenはstyle="display:none"と同じ動き
+/*
+    <div id="elem">A blinking element</div>
+
+    <script>
+        setInterval(() => elem.hidden = !elem.hidden, 1000);
+    </script>
+*/
+
+
+// その他のプロパティ
+// DOM要素には追加のプロパティ、特にクラスに依存するプロパティもある
+/*
+    // 例
+    <input type="text" id="elem" value="value">
+
+    <script>
+        alert(elem.type); // "text"
+        alert(elem.id); // "elem"
+        alert(elem.value); // value
+    </script>
+*/
