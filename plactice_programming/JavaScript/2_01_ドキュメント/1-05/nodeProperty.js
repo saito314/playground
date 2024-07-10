12aa15e00b2d8b733c0335b84765b32ab3c34e91
saito <saitou228@gmail.com>
2024-07-10 19:40:51 +0900

    add: 2-1-5途中

---
+++ plactice_programming/JavaScript/2_01_ドキュメント/1-05/nodeProperty.js
@@ -0,0 +1,120 @@
+"use strict";
+
+// DOMノードクラス
+// 異なるDOMノードは異なるプロパティを持つ
+// 階層ルートはEventTargetで、これはNodeにより継承されている
+// EventTarget - ルートの抽象クラス。このクラスのオブジェクトは生成されない
+// Node - DOMノードの基底として機能する。parentNode, nextSibling, childNodesなどの機能を提供する。
+// Elements - DOM要素のベースクラス。nextElementsSibling, children, getElementsByTagName, querySelector等の検索メソッドといった要素レベルのナビゲーションを提供する。
+// HTMLElement - 最終的にすべてのHTML要素のベースクラス。色々なHTML要素がこれを継承している。
+
+
+// DOMノードのクラスは通常constructorプロパティを持っている
+{
+    alert(document.body.constructor.name);
+}
+
+// もしくはそのtoString
+{
+    alert(document.body);
+}
+
+// また、継承チェックにinstanceofを使うこともできる
+{
+    alert(document.body instanceof HTMLBodyElement); // true
+    alert(document.body instanceof HTMLElement); // true
+    alert(document.body instanceof Element); // true
+    alert(document.body instanceof Node); // true
+    alert(document.body instanceof EventTarget); // true
+}
+
+// ブラウザ、console.dir(elem)を使った要素の出力でも簡単に見ることができる
+// コンソールでは、HTMLElements.prototype, Element.prototype
+
+
+// "nodeType"プロパティ
+// elem.nodeType == 1は要素ノード
+// elem.nodeType == 3はテキストノード
+// elem.nodeType == 9はドキュメントオブジェクト
+/*
+    例えば...
+    <body>
+        <script>
+        let elem = document.body;
+
+        // それが何かを調べてみる
+        alert(elem.nodeType); // 1 => element
+
+        // 最初の子は...
+        alert(elem.firstChild.nodeType);
+
+        // ドキュメントオブジェクトは、タイプ9
+        alert(document.nodeType);
+        </script>
+    </body>
+*/
+
+// タグ: nodeNameとtagName
+{
+    // 与えられたDOMノードに対して、nodeNameまたはtagNameプロパティでそのタグの名前を見ることができる
+    alert(document.body.nodeName);
+    alert(document.body.tagName);
+}
+
+/*
+    <body>
+        <script>
+            // for comment
+            alert(document.body.firstChild.tagName);
+            alert(document.body.firstChild.nodeName);
+
+            // for document
+            alert(document.tagName);
+            alert(document.nodeName);
+        </script>
+    </body>
+*/
+
+
+// innerHTML: コンテンツ
+// innerHTMLプロパティが要素内のHTMLを文字列として取得することができる
+/*
+    <body>
+        <p>A paragraph</p>
+        <div>A div</div>
+
+        <script>
+            alert(document.body.innerHTML); // 現在の内容を読む
+            document.body.innerHTML = "The new BODY!"; // 置き換える
+        </script>
+    </body>
+*/
+
+/*
+    無効なHTMLを挿入しようとすると、ブラウザはエラーを修正する
+    <body>
+        <script>
+            document.body.innerHTML = "<b>test"; // タグのクローズ忘れ
+            alert(document.body.innerHTML); // <b>test</b> (修正された)
+        </script>
+    </body>
+*/
+
+
+// 注意："innerHTML+="は完全な置き換えをする
+// elem.innerHTML+="何か"を使って"より多くのHTML"を追加することができる
+{
+    chatDiv.innerHTML += "<div>Hello<img src='smile.gif'/> !</div>";
+    chatDiv.innerHTML += "How goes?";
+}
+
+// 技術的には下記の2つは同じ
+{
+    elem.innerHTML += "...";
+    elem.innerHTML = elem.innerHTML + "..."
+}
+
+// この場合、innerHTMLは次のことを示す
+// 1. 古いコンテンツは削除される
+// 2. 代わりに新しいinnerHTMLが書かれる
+// コンテンツが完全に取り除かれ、最初から書き直されると全ての画像やその他のリソースがリロードされる
