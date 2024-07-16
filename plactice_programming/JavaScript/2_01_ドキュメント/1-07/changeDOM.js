ea449183e649ffefbf7efb38def2faf448d9b36f
saito <saitou228@gmail.com>
2024-07-16 23:34:56 +0900

    add: 2-1-7途中

---
+++ plactice_programming/JavaScript/2_01_ドキュメント/1-07/changeDOM.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// ドキュメントの変更
+// DOMの変更は動的なページ
+// 例：メッセージを表示
+// alertよりも見栄えの良いメッセージをページに追加する方法を考える
+/*
+    <style>
+        .alert {
+            padding: 15px;
+            border: 1px solid #d6e9c6
+            border-radius: 4px;
+            color: #3c763d;
+            background-color: #dff0d8;
+        }
+    </style>
+
+    <div class="alert">
+        <strong>Hi there!</strong> You've read an important message.
+    </div>
+*/
