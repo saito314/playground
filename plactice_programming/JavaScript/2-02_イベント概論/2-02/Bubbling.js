cf047ab96b1224cf9a9b1243a90bf56de91a3fb0
saito314 <saitou228@gmail.com>
2024-08-05 18:50:41 +0900

    update: 2-2-2完了

--- plactice_programming/JavaScript/2-02_イベント概論/2-02/Bubbling.js
+++ plactice_programming/JavaScript/2-02_イベント概論/2-02/Bubbling.js
@@ -29,3 +29,54 @@
         </div>
     </form>
 */
+// <p>をクリックすると3つのアラートが表示される
+// このプロセスをバブリングという
+
+
+// event.target
+// 親要素のハンドラは常に実際に発生した場所についての詳細を取得できる
+// イベントが発生した最も深くネストされた要素はターゲット要素と呼ばれ、event.targetでアクセスできる
+// this(=event.currentTarget)との違いは下記の通り
+// ・event.targetはイベントが開始されたターゲット要素で、バブリングプロセスを通して変化しない
+// ・thisは現在の要素で、現在実行中のハンドラをもつ
+
+
+// バブリングを止める
+// バブリングを止めるためのメソッドはevent.stopPropagation()
+/*
+    <body onclick="alert(`the bubbling doesn't reach here`)">
+        <button onclick="event.stopPropagation()">Click me</button>
+    </body>
+*/
+
+
+// キャプチャリング
+// 実際にはほとんど使われないが、役立つときもある。
+/*  
+    elem.addEventListener(..., {capture: true});
+    // または、単に"true"は{capture: true}のエイリアス
+    elem.addEventListener(..., true);
+*/
+
+// キャプチャリングとバブリング両方の例を見てみる
+/*
+    <style>
+        body * {
+            margin: 10px;
+            border: 1px solid blue;
+        }
+    </style>
+
+    <form>FORM
+        <div>DIV
+            <p>P</p>
+        </div>
+    </form>
+
+    <script>
+        for(let elem of document.querySelectorAll('*')) {
+            elem.addEventListener("click", e => alert(`Capturing: ${elem.tagName}`), true);
+            elem.addEventListener("click", e => alert(`Bubbling: ${elem.tagName}`));
+        }
+    </script>
+*/
