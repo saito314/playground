4e3a96aa3d93d3cbaa226b46029493382dfedcd7
saito314 <saitou228@gmail.com>
2024-08-14 23:40:11 +0900

    update: 2-2-4完了

--- plactice_programming/JavaScript/2-02_イベント概論/2-04/default.js
+++ plactice_programming/JavaScript/2-02_イベント概論/2-04/default.js
@@ -38,3 +38,82 @@
         return false;
     }
 }
+
+// さらなるイベントの防止
+// あるイベントは別のものへと流れる。最初のイベントを防いだ場合、2つめはない。
+/*
+    <input value="Focus works" onfocus="this.value=''">
+    <input onmousedown="return false" onfocus="this.value=''" value="Click me">
+*/
+
+// event.defaultPrevented
+// デフォルトアクションが防がれた場合、event.defaultPreventedはtrueで、それ以外はfalse
+// バブリングの停止が必要に見える例を見てみる
+// 実際にはバブリングを止める必要はない
+/*
+    <button>Right-click for browser context menu</button>
+
+    <button oncontextmenu="alert('Draw our menu'); return false">
+        Right-click for our context menu
+    </button>
+*/
+
+// 今、オプションを使用した独自のドキュメント全体のコンテキストメニューを実装したいとする
+// そしてドキュメント内には独自のコンテキストメニューを持つほかの要素がある可能性がある
+/*
+    <p>Right-click here for the document context menu</p>
+    <button id="elem">Right-click here for the button context menu</button>
+
+    <script>
+        elem.oncontextmenu = function(event) {
+            event.preventDefault();
+            alert("Button context menu");
+        };
+
+        document.oncontextmenu = function(event) {
+            event.preventDefault();
+            alert("Document context menu");
+        };
+    </script>
+*/
+
+// 問題はelemのクリック時2つのメニューを取得する：ボタンレベルとドキュメントレベルのメニュー
+// 修正方法はボタンハンドラの中でイベントを完全に処理し、それを止めることでevent.stopPropagation()を使う
+/*
+    <p>Right-click for the document menu</p>
+    <button id="elem">Right-click for the button menu (fixed with event.stopPropagation)</button>
+
+    <script>
+        elem.oncontextmenu = function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            alert("Button context menu");
+        };
+
+        document.oncontextmenu = function(event) {
+            event.preventDefault();
+            alert("Document context menu");
+        };
+    </script>
+*/
+
+// これでボタンレベルのメニューは期待通りに動作する
+// しかし、コストは高外部コードの右クリックに関する情報へのアクセスは拒否される
+/*
+    <p>Right-click for the document menu (fixed with event.defaultPrevented)</p>
+    <button id="elem">Right-click for the button menu</button>
+
+    <script>
+        elem.oncontextmenu = function(event) {
+            event.preventDefault();
+            alert("Button context menu");
+        };
+
+        document.oncontextmenu = function(event) {
+            if (event.defaultPrevented) return;
+
+            event.preventDefault();
+            alert("Document context menu");
+        };
+    </script>
+*/
