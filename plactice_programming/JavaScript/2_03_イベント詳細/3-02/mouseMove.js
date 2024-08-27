b11201f6c2137021aec35d563e49c5022531b14a
saito314 <saitou228@gmail.com>
2024-08-27 23:55:08 +0900

    update: 2-3-2完了

--- plactice_programming/JavaScript/2_03_イベント詳細/3-02/mouseMove.js
+++ plactice_programming/JavaScript/2_03_イベント詳細/3-02/mouseMove.js
@@ -13,3 +13,47 @@
 // mouseout
 // event.target: マウスが離れた要素
 // event.relatedTarget: 新たなポインタの下の要素
+
+
+// イベントの頻度
+// mousemoveイベントはマウスの移動時にトリガされる
+// しかし、すべてのピクセル単位の移動でイベントが発生するわけではない
+// 訪問者がマウスをとても早く動かしている場合、DOM要素はスキップされる可能性がある
+
+
+// 子へむけて移動するときの余分なmouseout
+// マウスポインタが動くことでmouseoverの後にmouseoutがトリガされる可能性がある
+// ブラウザのロジックによれば、マウスカーソルは常に単一の要素-最もネストされた要素-の上にだけある
+
+
+// イベントmouseenterとmouseleave
+// mouseenterとmouseleaveはmouseoverとmouseoutのようなもの
+// 違いが2つだけある
+// 1. 要素内の繊維はカウントされない
+// 2. イベントmouseenter/mouseleaveはバブルしない
+
+
+// イベント移譲
+// イベントmouseenter/leaveは非常に使いやすいが、バブルしないためイベントの移譲ができない
+// table上のmouseenter/leaveに対するハンドラはテーブル全体の出入りのみトリガする
+// そんな場合にはmouseover/mouseoutを使ってみる
+{
+    // マウスの下にあるセルをハイライトする
+    table.onmouseover = function(event) {
+        let target = event.target;
+        target.style.background = "pink";
+    };
+
+    table.onmouseout = function(event) {
+        let target = event.target;
+        target.style.background = "";
+    };
+}
+
+// これらのハンドラは任意の要素からテーブルの内側で行くときに動作する
+// しかし、全体として<td>に出入りする遷移のみを処理したいと考える。
+// そしてセル全体を強調する。tdの子の間で起こる遷移を処理したくない
+// 解決策は以下の通り
+// ・変数で現在強調されているtdを覚える
+// ・mouseoverではまだ現在のtdの中にいる場合はイベントを無視する
+// ・mouseoutでは現在のtdを離れなかった場合には無視する
