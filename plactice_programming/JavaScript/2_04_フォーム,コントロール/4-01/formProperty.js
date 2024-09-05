0ed862eadef3490dfe10f6e300a7c7f37d31e0c2
saito314 <saitou228@gmail.com>
2024-09-06 02:30:36 +0900

    update: 4-1-1完了

--- plactice_programming/JavaScript/2_04_フォーム,コントロール/4-01/formProperty.js
+++ plactice_programming/JavaScript/2_04_フォーム,コントロール/4-01/formProperty.js
@@ -70,3 +70,116 @@
             alert(fieldset.elements.login == form.elements.)
 */
 
+
+// より短い記法: form.name
+// form[index/name]でアクセスすることができる
+// form.elements.loginの代わりに、form.loginと書ける
+// もし要素にアクセスしたとでそのnameを変えた場合、依然として古い名前で利用可能
+/*
+    <form id="form">
+        <input name="login">
+    </form>
+
+    <script>
+        alert(form.elements.login === form.login); // true
+
+        form.login.name = "username"; // inputの名前を変更
+
+        // form.elementsはnameを更新:
+        alert(form.elements.login); // undefined
+        alert(form.elements.username); // input
+
+        // 直アクセスが両方の名前が利用可能
+        alert(form.username == form.login); // true
+    </script>
+*/
+
+
+// 後方参照: elements.form
+// 任意の要素に対して、フォームはelements.formとして利用可能
+// そのため、フォームはすべての要素を参照し、要素はフォームを参照する
+/*
+    <form id="form">
+        <input type="text" name="login">
+    </form>
+
+    <script>
+        // form -> element
+        let login = form.login;
+
+        // element -> form
+        alert(login.form); // HTMLFormElement
+    </script>
+*/
+
+
+// フォーム要素
+// それぞれの機能に注意を払いながら、フォームコントロールについて知る
+
+
+// inputとtextarea
+// 通常、チェックボックスに対してはinput.valueまたはinput.checkedで値にアクセスできる
+{
+    input.value = "New value";
+    textarea.value = "New text";
+
+    input.checked = true; // チェックボックスやラジオボタンの場合
+}
+
+// selectとoption
+// <select>要素は3つの重要なプロパティを持っている
+// 1. select.options - <option>要素の集合
+// 2. select.value - 選ばれた選択肢の値
+// 3. select.selectedIndex - 選ばれた選択肢の番号
+
+// したがって、<select>の値を設定するのに3つの方法がある
+// 1. 必要な<option>を見つけて、option.selectedをtrueに設定する
+// 2. select.valueに値を設定する
+// 3. select.selectedIndexにoptionの番号を設定する
+
+/*
+    <select id="select">
+        <option value="apple">Apple</option>
+        <option value="pear">Pear</option>
+        <option value="banana">Banana</option>
+    </select>
+
+    <script>
+        // この3行はすべて同じことをしている
+        select.options[2].selected = true;
+        select.selectedIndex = 2;
+        select.value = "banana";
+    </select>
+*/
+
+
+// ほかのコントロールとは違い、<select multiple>は複数の選択を許可する
+// この場合、すべての選択された値を取得するには、select.optionsを参照する必要がある
+/*
+    <select id="select" multiple>
+        <option value="blues" selected>Blues</option>
+        <option value="rock" selected>Rock</option>
+        <option value="classic">Classic</option>
+    </select>
+
+    <script>
+        // get all selected values form multi-select
+        let selected = Array.from(select.options)
+            .filter(option => option.selected)
+            .map(option => option.value);
+
+        alert(selected); // blues,rock
+    </script>
+*/
+
+
+// new Option
+// option要素には<option>要素を作成するための簡単でナイスな構文がある
+/*
+    option = new Option(text, value, defaultSelected, selected);
+*/
+
+// 例
+{
+    let option = new Option("Text", "value");
+}
