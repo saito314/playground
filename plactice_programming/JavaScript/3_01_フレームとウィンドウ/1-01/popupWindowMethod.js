29ab770a41b734988299c92e5577fdf42d8090ba
saito314 <saitou228@gmail.com>
2024-10-17 14:31:10 +0900

    update: 3-1-1完了

--- plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-01/popupWindowMethod.js
+++ plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-01/popupWindowMethod.js
@@ -45,3 +45,65 @@
     let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100`;
     open("/", "test", params)
 }
+
+
+// ポップアップにアクセスする
+// open呼び出しは新しいウィンドウへの参照を返す
+// それはプロパティを操作したり、位置を変えたりといったことをするのに利用できる
+{
+    let newWin = window.open("about:blank", "hello", "width=200,height=200");
+
+    newWin.document.write("Hello, world!");
+}
+
+// コンテンツのロード後に変更する
+{
+    let newWindow = open("/", "example", "width=300,height=300");
+    newWindow.focus();
+
+    alert(newWindow.location.href); // about:blank, 読み込みはまだ始まってない
+
+    newWindow.onload = function() {
+        let html = `<div style="font-size:30px">Welcome!</div>`;
+        newWindow.document.body.insertAdjacentHTML("afterbegin", html);
+    };
+}
+
+
+// 開いた元(opener)のウィンドウにアクセスする
+// ポップアップはwindow.opener参照を用いることでも"opener"ウィンドウにアクセスできる
+// ポップアップ以外のすべてのウィンドウの場合、それはnull
+{
+    let newWin = window.open("about:blank", "hello", "width=200, height=200");
+
+    newWin.document.write(
+        "<script>window.opener.document.body.innerHTML = 'Test'</script>"
+    );
+}
+
+// ポップアップを閉じる
+{
+    let newWindow = open("/", "example", "width=300,height=300");
+
+    newWindow.onload = function() {
+        newWindow.close();
+        alert(newWindow.closed); // true
+    };
+}
+
+
+// 移動とリサイズ
+// moveBy, moveTo, resizeBy, resizeTo
+// ただし、追加のタブのないポップアップに対してのみ確実に機能する
+
+
+// ウィンドウのスクロール
+// scrollBy, scrollTo, scrollIntoView
+
+
+// ポップアップのfocus/blur
+// 理論的にはウィンドウにフォーカスを当てる/外すfocus()/blur()メソッドがある
+// 実際はかなり制限されている。
+{
+    window.onblur = () => window.focus();
+}
