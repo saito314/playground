27881ae828e2dff82ab5c9427ab43b9c904106bd
saito314 <saitou228@gmail.com>
2024-10-22 23:46:27 +0900

    update: 3-1-2途中

--- plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-02/windowOrigin.js
+++ plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-02/windowOrigin.js
@@ -17,7 +17,7 @@
 // <script>あるいは<img>と同じような単なるタグである一方、ウィンドウ内のウィンドウ
 // ・iframe.contentWindowは<iframe>内のウィンドウへの参照
 // ・iframe.contentDocumentは<iframe>内のドキュメントへの参照
-/*
+/* 別のオリジン
     <iframe src="https://example.com" id="iframe"></iframe>
 
     <script>
@@ -44,5 +44,55 @@
 
             iframe.coload = null; // 一度だけ実行させるためにハンドラをクリア
         };
+    </script>
+*/
+
+
+/* 同一オリジン
+    <iframe src="/" id="iframe"></iframe>
+
+    <script>
+        iframe.onload = function() {
+            // なんでもできる
+            iframe.contentDocument.body.prepend("Hello, world!");
+        };
+    </script>
+*/
+
+
+// iframeがロードされるまで待ってください
+// iframeが作成されるとすぐにドキュメントを待つが、そのドキュメントは最終的にそこにロードされるものとは異なる
+/*
+    <iframe src="/" id="iframe"></iframe>
+
+    <script>
+        let oldDoc = iframe.contentDocument;
+        iframe.onload = function() {
+            let newDoc = iframe.contentDocument;
+            // ロードされたドキュメントは初期のものとは同じではない
+            alert(oldDoc == newDoc); // false
+        };
+    </script>
+*/
+
+
+// 同一オリジンの場合は次のように新しいドキュメントが現れる瞬間をとらえて、必要なハンドラの設定を試みることができる
+/*
+    <iframe src="/" id="iframe"></iframe>
+
+    <script>
+        let oldDoc = iframe.contentDocument;
+
+        // ドキュメントが新しいものか100ms毎にチェック
+        let timer = setInterval(() => {
+            if (iframe.contentDocument == oldDoc) return;
+
+            // 新しいドキュメントなので、ハンドラをセットする
+            iframe.contentDocument.addEventListener("DOMContentLoaded", () => {
+                iframe.contentDocument.body.prepend("Hello, world!");        
+            });
+
+            clearInterval(timer); // もう必要ないのでsetIntercvalをクリアする
+        }, 100);
     </script>
 */
