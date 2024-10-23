daa474a7e7da5cb84a309f679bc5ff95e7aaa405
saito314 <saitou228@gmail.com>
2024-10-23 10:30:47 +0900

    update: 3-1-2完了

--- plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-02/windowOrigin.js
+++ plactice_programming/JavaScript/3_01_フレームとウィンドウ/1-02/windowOrigin.js
@@ -1,6 +1,9 @@
 "use strict";
 
 
+// 誤字か？：対応する window オブジェクトを階層を形成します。
+
+
 // ウィンドウを跨いだやり取り
 // 同一オリジンポリシーはウィンドウとフレームのアクセスを互いに制限する
 
@@ -96,3 +99,67 @@
         }, 100);
     </script>
 */
+
+
+// window.frames
+// <iframe>のウィンドウオブジェクトを取得する別の方法は、名前付けされたコレクションwindow.framesから取得すること
+/* 例
+    <iframe src="/" style="height:80px" name="win" id="iframe"></iframe>
+
+    <script>
+        alert(iframe.contentWindow == frames[0]); // true
+        alert(iframe.contentWindow == frames.win); // true
+    </script>
+*/
+// iframeは内側に別のiframeを持つ場合がある
+// 対応するwindowオブジェクトの階層を形成する
+
+// 現在のドキュメントがフレームの中で開かれているかどうかを確認するのにtopプロパティが使える
+/*
+    if(window == top) {
+        alert("The script is in the topmost window, not in a frame");
+    } else {
+        alert("The script tuns in a frame!"); 
+    }
+*/
+
+
+// The sandbox attribute
+// sandbox属性が信頼できないコードが実行されるのを防ぐため、<iframe>内の特定のアクションを除外できる
+// それらを別のオリジンからくるものとして扱うことによって、またはほかの制限を適用することでiframeをサンドボックス化する
+
+
+// ウィンドウを跨いだメッセージング
+// postMessageインターフェースはどのオリジンから来ていたとしても、ウィンドウ同氏がやり取りするのを可能にする
+/*
+    <iframe src="http://example.com" name="example">
+
+    <script>
+        let win = window.frames.example;
+
+        win.postMessage("message", "http://example.com");
+    </script>
+*/
+/* チェックしたくない場合にはtargetOriginに*を設定する
+    <iframe src="http://example.com" name="example">
+
+    <script>
+        let win = window.frames.example;
+
+        win.postMessage("message", "*");
+    </script>
+*/
+
+// onmessage
+// メッセージを受け取るためにはターゲットウィンドウはmessageイベントのハンドラが必要
+// これはpostMessageが呼び出されたときに実行される
+{
+    window.addEventListener("message", function(event) {
+        if (event.origin != "http://javascript.info") {
+            // 未知のドメインからの場合は無視する
+            return;
+        }
+
+        this.alert("received: " + event.data);
+    });
+}
