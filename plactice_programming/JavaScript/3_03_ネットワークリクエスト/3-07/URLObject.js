44ca579e931d9027f249bcd6990f43d828163789
saito314 <saitou228@gmail.com>
2024-12-15 01:23:25 +0900

    update: 3-3-7完了

--- plactice_programming/JavaScript/3_03_ネットワークリクエスト/3-07/URLObject.js
+++ plactice_programming/JavaScript/3_03_ネットワークリクエスト/3-07/URLObject.js
@@ -7,3 +7,36 @@
 {
     new URL(url, [base])
 }
+
+
+// HTTP認証がある場合にはuserとpasswordプロパティがある
+// 相対URLを作成するときにも利用できる
+// この場合は2つめの引数を使う
+{
+    let url = new URL('profile/admin', 'https://javascript.info');
+
+    alert(url);
+
+    url = new URL('tester', url);
+
+    alert(url);
+}
+
+// SearchParams
+// 与えられた検索パラメータでurlを作成したいとする
+// これらは正しくエンコードされている必要がある
+// URLオブジェクトはurlパラメータを操作するための簡単な方法も提供する
+{
+    let url = new URL('https://google.com/search');
+    url.searchParams.set("query", "test me!");
+
+    alert(url);
+
+    url.searchParams.set("tbs", "adr:y");
+
+    alert(url);
+
+    for(let [name, value] of url.searchParams) {
+        alert(`${name}=${value}`);
+    }
+}
