0967c3fceec1642122a8b94b2d13a78cc5db2a25
saito314 <saitou228@gmail.com>
2025-01-17 23:45:58 +0900

    update: 3-3-2途中

--- plactice_programming/JavaScript/3_04_ブラウザへのデータ保存/4-02/localStorage.js
+++ plactice_programming/JavaScript/3_04_ブラウザへのデータ保存/4-02/localStorage.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// localstorageのデモ
+// 主な特徴は
+// ・同じオリジンからのすべてのタブとウィンドウ間で共有される
+// ・データは期限切れにならない
+{
+    localStorage.setItem("test", 1);
+}
+
+// その後ブラウザを閉じる/開く、あるいは別のウィンドウで同じページを開いた後、次のようにしてその値を取得できる
+{
+    alert(localStorage.getItem("test"));
+}
