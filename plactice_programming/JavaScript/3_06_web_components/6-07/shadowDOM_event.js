d7271e9ae7adbe9e750aa36dd0f9bd12bb21c67d
saito314 <saitou228@gmail.com>
2025-02-15 23:44:17 +0900

    update: 3-6-7更新

--- plactice_programming/JavaScript/3_06_web_components/6-07/shadowDOM_event.js
+++ plactice_programming/JavaScript/3_06_web_components/6-07/shadowDOM_event.js
@@ -1,1 +1,24 @@
 "use strict";
+
+
+// ShadowDOMとイベント
+// shadowDOM内で発生するイベントがコンポーネントの外でキャッチされると、そのイベントはターゲットとしてshadowホスト要素を持つ
+/*
+    <user-card></user-card>
+
+    <script>
+    customElements.define("user-card", class extends HTMLElement {
+        connectedCallback() {
+            this.attachShadow({mode: "open"});
+            this.shadowRoot.innerHTML = `<p>
+                <button>Click me</button>
+            </p>;
+            this.shadowRoot.firstElmentChild.onclick = 
+                e = > alert("Inner target: " + e.target.tagName);
+        }
+    });
+
+    document.onclick =
+        e => alert("Outer target: " + e.target.tagName);
+    </script>
+*/
