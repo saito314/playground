ff174e3ed3152c9fbb958df42dc3a1aca47891cf
saito314 <saitou228@gmail.com>
2025-02-28 00:12:53 +0900

    update: 3-7-5完了

--- plactice_programming/JavaScript/3_07_正規表現/7-05/anchor.js
+++ plactice_programming/JavaScript/3_07_正規表現/7-05/anchor.js
@@ -10,3 +10,28 @@
 
     alert(str.match(/^\d+/gm))
 }
+// mフラグがない場合は、最初の数値だけがマッチする
+{
+    let str = `1st place: Winnie
+        2nd place: Piglet
+        3rd place: Eeyore`;
+
+    alert(str.match(/^\d+/g));
+}
+
+// 行の終わり$の検索
+{
+    let str = `Winnie: 1
+        Piglet: 2
+        Eeyore: 3`;
+
+    alert(str.match(/\d$/gm));
+}
+// ^$の代わりに\nを検索する
+{
+    let str = `Winnie: 1
+        Piglet: 2
+        Eeyore: 3`;
+
+    alert(str.match(/\d\n/gm));
+}
