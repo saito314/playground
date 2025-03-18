cca5e9b5b65b284a119239f85521993bba7f165c
saito314 <saitou228@gmail.com>
2025-03-18 23:40:13 +0900

    update: 3-7-7完了

--- plactice_programming/JavaScript/3_07_正規表現/7-07/escape.js
+++ plactice_programming/JavaScript/3_07_正規表現/7-07/escape.js
@@ -22,3 +22,39 @@
     alert("1\\2".match(/\\/));
 }
 
+
+// スラッシュ
+// スラッシュ記号は特殊文字ではありませんが、JavaScriptでは正規表現の開始と終了で使われるのでこれもエスケープが必要
+{
+    alert("/".match(/\//));
+}
+
+// new RegExp構文を利用する場合、エスケープする必要はない
+{
+    alert("/".match("/"));
+}
+
+
+// new RegExp
+// new RegExpで正規表現を作成している場合、/はエスケープする必要はありませなんが、エスケープが必要なものもいくつかある
+{
+    let reg = new RegExp("\d\.\d");
+
+    alert("Chapter 5.1".match(reg));
+}
+
+// \d\.\dが受け取られるか
+{
+    alert("\d\.\d");
+}
+
+// new RegExpの呼び出しは、バックスラッシュのない文字列を取得する
+// これを直すには引用符で\\を\にするためにバックスラッシュを二重にする必要がある
+{
+    let regStr = "\\d\\.\\d";
+    alert(regStr);
+
+    let reg = new RegExp(regStr);
+
+    alert("Chapter 5.1".match(reg));
+}
