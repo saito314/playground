036a0b432aa044b7c4292ae2983894404c536d8a
saito314 <saitou228@gmail.com>
2025-04-17 23:58:45 +0900

    update: 3-7-13完了

--- plactice_programming/JavaScript/3_07_正規表現/7-13/Alternation.js
+++ plactice_programming/JavaScript/3_07_正規表現/7-13/Alternation.js
@@ -1,1 +1,21 @@
-"use strict"
+"use strict";
+
+// 論理和指定子
+// 正規表現では|で表現される
+{
+    let reg = /html|php|css|java(script)?/gi;
+
+    let str = "First HTML appeared, then CSS, then JavaScript";
+
+    alert(str.match(reg));
+}
+
+
+// 時間の正規表現
+// 時間を正規表現で取得するときに\d\d:\d\dのような表現は、25:99なども許容してしまう
+// 論理和指定子を用いて[01]\d | 2[0-3]:[0-5]\dのように表現できる
+{
+    let reg = /([01]\d|2[0-3]):[0-5]\d/g;
+
+    alert("00:00 10:10 23:59 25:99 1:2".match(reg));
+}
